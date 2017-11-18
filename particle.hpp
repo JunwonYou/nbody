f18bf933414a35a4aa637f0685930dfc34da6f01
nammyoungjin <33752068+nammyoungjin@users.noreply.github.com>
2017-11-18 17:12:24 +0900

    Update and rename particle.h to particle.hpp

---
+++ particle.hpp
@@ -0,0 +1,30 @@
+
+
+class particle
+{
+    friend class particleset;
+public:
+    particle();
+    particle(double, double, double, double = 0.0, double = 0.0);
+    particle(particle&);
+    ~particle();
+    void set_m(double mass) { m = mass; }
+    void set_p(double px, double py) { x = px; y = py; }
+    void set_v(double pvx, double pvy) { vx = pvx; vy = pvy; }
+    double get_m() { return m; }
+    double get_x() { return x; }
+    double get_y() { return y; }
+    double get_vx() {return vx; }
+    double get_vy() { return vy; };
+    void printall();
+    
+private:
+    double m;
+    double x;
+    double y;
+    double vx;
+    double vy;
+    double f = 0;
+    particle* id = this;
+    
+};
