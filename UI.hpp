0e20aae3170a891489242a9dda433deb6aae2ac9
nammyoungjin <33752068+nammyoungjin@users.noreply.github.com>
2017-11-20 23:02:11 +0900

    Update UI.hpp

--- UI.hpp
+++ UI.hpp
@@ -1,5 +1,5 @@
 #include <string>
 using namespace std;
-void UI(string);
-string* split(string com);
-string trim(string& );
+void UI(string); // UI call
+string* split(string com); // 입력받은 스트링을 분할해서 반환
+string trim(string& ); // 입력받은 스트링의 앞뒤 빈칸을 삭제
